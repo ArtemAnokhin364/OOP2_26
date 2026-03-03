8ea710eea230c107bcef4c96538ded4d9e0d990f
ArtemAnokhin364 <artemanohin11@gmail.com>
2026-03-03 19:22:29 +0100

    punkty i odcinki

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,9 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    public double x, y;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.x = 50.0;
+        p1.y = 50.0;
+        System.out.println(p1);
+        System.out.println(p1.toSvg());
+
+        p1.translate(10, -20 );
+        System.out.println(p1);
+        System.out.println(p1.toSvg());
+
+        Point p2 = p1.translated(-30, 40);
+        System.out.println(p2);
+        System.out.println(p2.toSvg());
+
+        Segment s1 = new Segment();
+        s1.a = p1;
+        s1.b = p2;
+
+        System.out.println("długość s1 = " + s1.length());
+        System.out.println(s1);
+        System.out.println(s1.toSvg());
     }
 }
