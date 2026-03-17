d81c1f8acc50f3980221ee09bfdb65bb2da3c90d
ArtemAnokhin364 <artemanohin11@gmail.com>
2026-03-17 17:50:59 +0100

    dodano SvgScene

---
+++ svg/src/SvgScene.java
@@ -0,0 +1,22 @@
+public class SvgScene {
+    private Polygon[] polygons;
+    private int i = 0;
+    public SvgScene() {
+        polygons = new Polygon[3];
+    }
+
+    public void addPolygon(Polygon poly) {
+        polygons[i] = poly;
+        i = (i + 1) % 3; // 0 -> 1 -> 2 -> 0 -> 1 -> 2 -> ...
+    }
+
+    public String toSvg() {
+        String svg = "";
+        for (Polygon p : polygons) {
+            if (p == null) continue;
+
+            svg += p.toSvg() + "\n";
+        }
+        return svg;
+    }
+}
