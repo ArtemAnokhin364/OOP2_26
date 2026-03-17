b803776e835b8b25cb34284a75a52ba4523c743b
ArtemAnokhin364 <artemanohin11@gmail.com>
2026-03-17 18:13:30 +0100

    BoundingBox rysunku

--- svg/src/SvgScene.java
+++ svg/src/SvgScene.java
@@ -1,6 +1,23 @@
 public class SvgScene {
     private Polygon[] polygons;
     private int i = 0;
+
+    private BoundingBox boundingBox() {
+        double minWidth = 0;
+        double minHeight = 0;
+        for (Polygon poly : polygons) {
+            if (poly == null) continue;
+
+            BoundingBox bb = poly.boundingBox();
+            double width = bb.x() + bb.width();
+            if (width > minWidth) minWidth = width;
+            double height = bb.y() + bb.height();
+            if (height > minHeight) minHeight = height;
+        }
+
+        return new BoundingBox(0, 0, minWidth, minHeight);
+    }
+
     public SvgScene() {
         polygons = new Polygon[3];
     }
