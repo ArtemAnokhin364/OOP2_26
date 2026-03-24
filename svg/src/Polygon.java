60599984963c6f5ff6698276527f453a4dc99171
ArtemAnokhin364 <artemanohin11@gmail.com>
2026-03-24 17:50:37 +0100

    dziedziczenie i abstrakcja

--- svg/src/Polygon.java
+++ svg/src/Polygon.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
 import java.util.Locale;
 
-public class Polygon {
+public class Polygon extends Shape {
     private Point[] points;
-    private Style style;
 
     // metoda wytwórcza: statyczna metoda zwracająca otworzony obiekt
     // nie jest to konstruktor
@@ -36,11 +35,12 @@ public class Polygon {
     }
 
     public Polygon(Point[] points, Style style) {
+        // super() wywołuje konstruktor klasy nadrzędnej
+        super(style);
         this.points = new Point[points.length];
         for (int i = 0; i < points.length; i++) {
             this.points[i] = new Point(points[i]);
         }
-        this.style = style;
     }
 
     @Override
